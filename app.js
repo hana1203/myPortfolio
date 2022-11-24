686da3dddfb4aa7f0e67f0a7a74c8c3d4892b4ef
hana1203 <hanaindec@gmail.com>
2022-11-24 23:55:38 +0900

    icon map으로 넣기

--- app.js
+++ app.js
@@ -15,15 +15,63 @@ function App() {
       srclink:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     },
+
+    {
+      name: "React",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
+    },
+    {
+      name: "Redux Toolkit",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+    },
+    {
+      name: "Styled-components",
+      srclink:
+        "https://raw.githubusercontent.com/styled-components/brand/master/styled-components.png",
+    },
+    {
+      name: "Java",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+    },
+    {
+      name: "Python",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    },
+    {
+      name: "Git",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+    {
+      name: "Github",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    {
+      name: "Figma",
+      srclink:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    },
+    {
+      name: "Notion",
+      srclink:
+        "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png",
+    },
   ];
 
-  const mappedIcon = iconCollection.map(
-    (el, idx) =>
-      `<div class="icon-box" key=${idx}>
-    <div class="devicon"><img src= ${el.srclink} /></div>
-    <span class="devicon-comment">${el.name}</span>
-    <div>`
-  );
+  const mappedIcon = iconCollection
+    .map(
+      (el) =>
+        `<div class="icon-box">
+      <div class="devicon"><img src= ${el.srclink} /></div>
+      <span class="devicon-comment">${el.name}</span>
+    </div>`
+    )
+    .join("");
 
   document.querySelector("#icon-container").innerHTML = mappedIcon;
 }
